2ee4587a0743a001ae0f07c14e7a71abe6342add
Evin <evinelqueley@gmail.com>
2023-02-24 08:41:09 -0400

    Primneiro commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+    <title>Resgate de Conteúdo</title>
+</head>
+
+<body>
+
+    <h1>Resgate de Conteúdo</h1>
+
+    <form action="recebeDados.php" method="POST">
+        Nome:<input type="text" name="nome"> </br>
+        Idade:<input type="number" name="idade"> </br>
+        Sexo:
+        <select>
+            <option>Masculino</option>
+            <option>Feminino</option>
+        </select> </br> </br>
+        <input type="submit" value="Salvar">
+    </form>
+</body>
+
+</html>
